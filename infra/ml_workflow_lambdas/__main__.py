3124ab54b1658d80c3575b2c390d14e76836a854
davidfox <foxy1987@gmail.com>
2022-08-31 21:51:27 -0600

    automatically push docker image to ecr and deploy container-based lambda image

--- infra/ml_workflow_lambdas/__main__.py
+++ infra/ml_workflow_lambdas/__main__.py
@@ -1,10 +1,55 @@
 """An AWS Python Pulumi program"""
 
-import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
+import pulumi_docker as docker
+import json
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+repo = aws.ecr.Repository("churn-train")
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+ecr_creds = aws.ecr.get_authorization_token()
+
+lambda_image = docker.Image("xgboost-train-dockerimage",
+    image_name=repo.repository_url,
+    build="./steps",
+    skip_push=False,
+    registry=docker.ImageRegistry(server=repo.repository_url, username=ecr_creds.user_name, password=ecr_creds.password)
+)
+
+iam_role_lambda = aws.iam.Role(
+            "exec-role",
+            assume_role_policy=json.dumps(
+                {
+                    "Version": "2012-10-17",
+                    "Statement": [
+                            {
+                            "Effect": "Allow",
+                            "Principal": { "Service": "lambda.amazonaws.com"},
+                            "Action": "sts:AssumeRole"
+                            }
+                        ]
+                    }
+            ),
+            inline_policies=[aws.iam.RoleInlinePolicyArgs(name='s3-access',
+                                                          policy='''{"Version":"2012-10-17",
+                                                                "Statement":[
+                                                                    {
+                                                                        "Effect":"Allow",
+                                                                        "Action":[
+                                                                            "s3:GetObject"
+                                                                        ],
+                                                                        "Resource":[
+                                                                            "arn:aws:s3:::churn-dataset",
+                                                                            "arn:aws:s3:::churn-dataset/*"
+                                                                        ]
+                                                                    }
+                                                                ]
+                                                                }'''
+                                                          )],
+)
+
+test_lambda = aws.lambda_.Function("testLambda",
+                                    package_type="Image",
+                                    role=iam_role_lambda.arn,
+                                    image_uri=lambda_image.image_name,
+                                    timeout=900
+    )
